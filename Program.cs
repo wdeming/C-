9a7fab0bb5bb3684fc36e3cb58fb4ce4b2ad659d
Leepree <talibjanov.k.n@gmail.com>
2022-06-26 02:10:50 +0500

    3 задача

--- Program.cs
+++ Program.cs
@@ -17,10 +17,33 @@ else if (first_number < second_number){
 }
 else Console.WriteLine("числа равны");
 
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Задача 4:Решение__________");
 int max = first_number;
 if (second_number > first_number)
     max = second_number;
 if (third_number > max)
     max = third_number;
 Console.WriteLine("Максимальным числом из укаханных является число:" + max);
+// ну или 
+// int max = new[] { first_number, second_number, third_number }.Max();
+// Console.WriteLine("Максимальным числом из укаханных является число:" + max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Задача 6:Решение__________"); // для первого вводимого числа
+if (first_number%2 == 0) Console.WriteLine("Первое введенное число является четным");
+else Console.WriteLine("Первое введенное число НЕ является четным");
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Задача 8:Решение__________"); // для первого вводимого числа
+int module = Math.Abs(Convert.ToInt32(first_number));
+int count = 1;
+if (first_number > -2 &&  first_number < 2) Console.WriteLine("Между числами " + first_number + " и 0 нет четных чисел");
+else {
+    while (count <= module){     
+        if (count%2 == 0){
+            Console.WriteLine(first_number > 1 ? count : -count);
+        }
+        count++;
+    }    
+}
