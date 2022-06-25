0e5f2c3257c2ee67be30bb0c161f783a7a3b1144
Leepree <talibjanov.k.n@gmail.com>
2022-06-26 02:22:40 +0500

    fix lable

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Введите первое число");
 int first_number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите второе число");
 int second_number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int third_number = Convert.ToInt32(Console.ReadLine());
